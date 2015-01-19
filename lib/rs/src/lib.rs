1de46904784ad5f819a0960284ac71e6ce5d64e9
Simon Génier <s@simon.coffee>
2015-01-18 21:14:01 -0500

    Error handling for the read end of the protocol.

--- lib/rs/src/lib.rs
+++ lib/rs/src/lib.rs
@@ -6,3 +6,18 @@ pub use transport::Transport;
 
 pub mod protocol;
 pub mod transport;
+
+#[derive(Eq, PartialEq, Show)]
+pub enum ThriftErr {
+    TransportError(std::io::IoError),
+    UnknownProtocol,
+    InvalidData,
+    NegativeSize,
+    SizeLimit,
+    BadVersion,
+    NotImplemented,
+    DepthLimit,
+    InvalidUtf8(std::str::Utf8Error),
+}
+
+pub type TResult<T> = Result<T, ThriftErr>;
